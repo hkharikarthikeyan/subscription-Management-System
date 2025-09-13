e8197fe44461b5ea8f18a146104d19a7cc3c1721
shalinisubramaniam <shalinisubramaniam234@gmail.com>
2025-09-13 10:35:59 +0530

    Initial Spring Boot setup

---
+++ src/test/java/com/lumen/subscription_management/SubscriptionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumen.subscription_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
